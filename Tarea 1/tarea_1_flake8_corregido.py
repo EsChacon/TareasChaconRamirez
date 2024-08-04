a0a81e415f48807385ae62b2d875de6cd138d4a0
EsChacon <eschacon@estudiantec.cr>
2024-08-03 21:22:01 -0600

    Documentacion tarea_1_flake8_corregido.py
    Se agregaron comentarios explicando el metodo

--- Tarea 1/tarea_1_flake8_corregido.py
+++ Tarea 1/tarea_1_flake8_corregido.py
@@ -1,9 +1,12 @@
 def PWM():
+    # Contador de 0 a 255
     sum = 0
+    # Variable de conteo actual
     while True:
         sum = sum + 1
         if sum > 255:
             return None
+            # Al superar 255 reinicia el conteo
 
 
 PWM()
